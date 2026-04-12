5fb1c1323a4ac72f2af963382742c679a8ad371d
Pradi8 <prathab8.4@googlemail.com>
2026-04-12 15:06:51 +0200

    models
    model Quiz
    model Question

--- quizly_app/models.py
+++ quizly_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Quiz(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    video_url = models.URLField(blank=True, null=True)
+
+class Question(models.Model):
+    question = models.ForeignKey(Quiz, related_name='questions', on_delete=models.CASCADE)
+    question_title = models.CharField(max_length=255)
+    question_options = models.Choices(
+        ('Option A', 'Option A'),
+        ('Option B', 'Option B'),
+        ('Option C', 'Option C'),
+        ('Option D', 'Option D')
+    )
+    answer = models.OneToOneField(question_options, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
